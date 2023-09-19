8013bf3f1ed67fe45e1e99f3135d1ad0d44b129e
qormoon <qormoon@sookmyung.ac.kr>
2023-09-19 11:54:37 +0900

    두 번째 과제

--- week02/django02/apps/views.py
+++ week02/django02/apps/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
   return render(request, 'index.html')
 
+def intro(request):
+  return render(request, 'intro.html')
+
--- week02/django02/mysite/urls.py
+++ week02/django02/mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.views import index
+from apps.views import index,intro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hungry/', index),
+    path('sleep/', intro),
+
 ]

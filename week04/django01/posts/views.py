9e2b46bc528d1258d6c25b571761cf3b1981cba8
baek <qormoon@sookmyung.ac.kr>
2023-11-02 17:20:02 +0900

    5주차 과제

--- week04/django01/posts/views.py
+++ week04/django01/posts/views.py
@@ -5,10 +5,42 @@ from django.views.generic import ListView
 from .models import Post
 
 def url_view(request):
-    return HttpResponse('url.view')
+    data = {'code': '001', 'msg': 'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+
+def url_parameter_view(request, username):
+    print('url_parameter_view()')
+    print(username)
+    print(request.GET)
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method: {request.method}')
+    if request.method == "GET":
+        print(f'request.GET: {request.GET}')
+    elif request.method == 'POST':
+        print(f'request.POST: {request.POST}')
+    return render(request, 'view.html')
 
 class class_view(ListView):
     model = Post
     template_name = 'cbv_view.html'
 
+def index(request):
+    return render(request, 'index.html')
+
+def post_list_view(request):
+    return render(request, 'posts/post_list.html')
+
+def post_detail_view(request, id):
+    return render(request, 'posts/post_detail.html')
+
+def post_create_view(request):
+    return render(request, 'posts/post_form.html')
+
+def post_update_view(request, id):
+    return render(request, 'posts/post_update.html')
+
+def post_delete_view(reqeust, id):
+    return render(reqeust, 'posts/post_confirm_delete.html')
 # Create your views here.
